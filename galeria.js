d8b95c6594e20c876e4421ad0f02bce4a08e895b
Dyego Castro <dcastrocerqueira33@gmail.com>
2023-04-02 02:56:14 -0300

    Atualizado

--- galeria.js
+++ galeria.js
@@ -3,13 +3,23 @@ function modoEscuro(){
        document.querySelector("#dark").innerHTML = 'Light'
        document.querySelector("body").style.transition = '1s'
        document.querySelector("body").style.backgroundColor = '#000'
-       document.querySelector("body").style.color = '#fff'
-       document.querySelector(".formulario").style.border = '1px solid #fff'
+     
     }else if(document.querySelector("#dark").innerHTML == "Light"){
         document.querySelector("#dark").innerHTML = 'Dark'
         document.querySelector("body").style.transition = '1s'
         document.querySelector("body").style.backgroundColor = '#fff'
-        document.querySelector("body").style.color = '#000'
-        document.querySelector(".formulario").style.border = '1px solid #000'
+    
     }
+}
+
+function colocarMouse(){
+    document.querySelector(".galeria").style.transition = '1s'
+    document.querySelector(".galeria").style.boxShadow = "1px 1px 1px"
+    document.querySelector('.galeria').style.backgroundColor = '#ccc'
+}
+
+function tirarMouse(){
+    document.querySelector(".galeria").style.transition = '1s'
+    document.querySelector(".galeria").style.boxShadow = "0px 0px 0px"
+    document.querySelector('.galeria').style.backgroundColor = '#fff'
 }
